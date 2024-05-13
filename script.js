52cfcc76b29f1de18d1d81962f183bb68e5e86fe
Gabriele <124803038+SirPtavius@users.noreply.github.com>
2024-05-13 17:11:48 +0200

    improved header and page layout, added popup form with animation, started card layout

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.querySelector("#newBook").addEventListener("click", function () {
+  document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .cancel").addEventListener("click", function () {
+  document.querySelector(".popup").classList.remove("active");
+});
+
+const bookImg = document.getElementById("bookImg");
+bookImg.addEventListener("click", function () {
+  if (bookImg.classList.contains("active")) {
+    bookImg.src = "icon/open-book.png";
+    bookImg.classList.remove("active");
+  } else {
+    bookImg.classList.add("active");
+    bookImg.src = "icon/close-book.png";
+  }
+});
 const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,7 +26,7 @@ function Book(title, author, pages, read) {
   };
 }
 function addBookToLibrary() {
-  // do stuff here
+  //
 }
 const theHobbit = new Book(
   "The Hobbit",
